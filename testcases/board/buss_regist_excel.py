1c8f365bd10bc6577f12d0a5b949a05ae8ab35f0
yangmei21 <1154372391@qq.com>
2024-01-15 18:36:24 +0800

    优化了从excel多条数据加入的方法！！终于成功o(*￣▽￣*)ブ

--- testcases/board/buss_regist_excel.py
+++ testcases/board/buss_regist_excel.py
@@ -13,7 +13,6 @@ from testcases.test_admin_login import TestAdminLogin
 from util import util
 
 
-# 数据是通过excel文件读入后存入data中调用写入，每次需要在文件excel中来修改
 class Test_regist_buss(object):
     def read_excel(file_path):
         workbook = openpyxl.load_workbook(file_path)
@@ -54,17 +53,19 @@ class Test_regist_buss(object):
         注册商家信息提交
         :return:
         """
+        self.login.driver.refresh()
+        sleep(2)
         # 获取商家注册列表数
         total_regist = util.get_total(self.login.driver, '.ant-pagination-total-text')
-        print('该列表总数：', total_regist)
+        print('\n该列表总数：', total_regist)
 
         # 点击新建按钮
         self.login.driver.find_element(By.XPATH,
-                                       '/html/body/app-root/div/app-default/app-def-layout-content/nz-layout/nz-layout/nz-layout/nz-content/div/div/app-dept/div/app-card-table-wrap/nz-card/div[1]/div/div[2]/div/div[1]/button').click()
+                                       "//button[@nztype='primary']").click()
         sleep(2)
         # 页面最大化
-        self.login.driver.find_element(By.XPATH,
-                                       '//*[@id="cdk-overlay-2"]/nz-modal-container/div/div/button/span/div/span[1]').click()
+        # self.login.driver.find_element(By.CSS_SELECTOR,
+        #                                'span.hover-blue.full-height.flex-auto.text-right.d-i-b.ng-tns-c89-0 i.anticon-fullscreen').click()
 
         # 上传法人身份证
         upload_element = self.login.driver.find_element(By.XPATH,
@@ -84,12 +85,12 @@ class Test_regist_buss(object):
         self.login.driver.find_element(By.NAME, 'legalRepresentative').send_keys(test_data["Represen"])
         # 经营状态选择
         self.login.driver.find_element(By.XPATH,
-                                       '//*[@id="cdk-overlay-2"]/nz-modal-container/div/div/div[2]/app-dept-manage-modal/form/div[3]/div[2]/nz-form-item/nz-form-control/div/div/nz-select').click()
+                                       '//*[@id="cdk-overlay-2"]/nz-modal-container/div/div/div[2]/app-dept-manage-modal/form/div[3]/div[2]/nz-form-item/nz-form-control/div/div/nz-select/nz-select-top-control').click()
         sleep(1)
 
         options = WebDriverWait(self.login.driver, 5).until(
             EC.visibility_of_all_elements_located((By.XPATH,
-                                                   '//*[@id="cdk-overlay-3"]/nz-option-container/div/cdk-virtual-scroll-viewport/div[1]/nz-option-item/div'))
+                                                   '//*[@id="cdk-overlay-3"]/nz-option-container/div/cdk-virtual-scroll-viewport/div[1]/nz-option-item'))
         )
 
         # 定位并选择自己想要勾选的分类
@@ -120,10 +121,11 @@ class Test_regist_buss(object):
 
         # 验证是否新增成功
         assert total_regist + 1 == total_add_regist
+        sleep(3)
 
     def teardown_class(self):
         self.login.driver.quit()
 
 
 if __name__ == '__main__':
-    pytest.main(['buss_regist'])
+    pytest.main(['-vs', 'test_add_buss_from excel', '--reruns=2'])
